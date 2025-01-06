f7424ca855723812c7b8c654e80aba0214f00906
UPSkill.com Pvt LMT <rushikeshgaikwad2565@gmail.com>
2025-01-06 19:54:18 +0530

    Update JDBCDemo.java

--- MysqlProject/src/corejava/JDBCDemo.java
+++ MysqlProject/src/corejava/JDBCDemo.java
@@ -7,23 +7,22 @@ import java.sql.Statement;
 
 public class JDBCDemo {
     public static void main(String[] args) {
-        // Database credentials
-        String url = "jdbc:mysql://localhost:3306/employee_management_system"; // Replace DemoDB with your database name
+       
+        String url = "jdbc:mysql://localhost:3306/employee_management_system";
         String username = "root"; // Replace with your MySQL username
         String password = "Rushi123@#"; // Replace with your MySQL password
 
-        // JDBC Connection
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
             System.out.println("Connected to the database!");
 
-            // Create a statement
+       
             Statement statement = connection.createStatement();
 
-            // Query to fetch tables
+          
             String query = "SHOW TABLES;";
             ResultSet resultSet = statement.executeQuery(query);
 
-            // Display tables
+          
             System.out.println("Tables in the database:");
             while (resultSet.next()) {
                 System.out.println(resultSet.getString(1));
